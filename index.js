7d18d0932c2e7faed3d65de735d9f4bd177cb94f
samvat7 <samvattiwari_2k19ae053@dtu.ac.in>
2023-08-04 22:06:55 +0530

    register functionality added

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
+const cookieParser = require('cookie-parser');
+
 const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
 const port = 8000;
+
+const db = require('./config/mongoose');
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
