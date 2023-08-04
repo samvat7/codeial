5a4158f3e15dca352f1ba64e9c8b823556c8fd37
samvat7 <samvattiwari_2k19ae053@dtu.ac.in>
2023-08-04 22:06:36 +0530

    register functionality added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,11 @@
+const User = require('../models/users');
+
 module.exports.users = function(req,res){
 
-    res.end(`<h1>Users</h1>`);
+    res.render('users', {
+
+        title: "Codeial Users"
+    });
 }
 
 module.exports.profile = function(req,res){
@@ -13,4 +18,56 @@ module.exports.profile = function(req,res){
 module.exports.posts = function(req,res){
 
     res.end(`<h1>User's Posts</h1>`);
+}
+
+module.exports.register = function(req, res){
+
+    return res.render('register', {
+
+        title: "Codeial | Register"
+    });
+};
+
+module.exports.login = function(req, res){
+
+    return res.render('login', {
+        title: "Codeial | Log In"
+    });
+};
+
+//get the registeration data
+module.exports.create = function(req,res){
+
+    console.log(req.body);
+
+    if(req.body.password != req.body.confirmpassword){
+
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}).then(function(err, user){
+
+        if(err){ console.log('Error finding user in signing up.'); return}
+
+        if(!user){
+
+            User.create(req.body).then(function(err, user){
+
+                return res.redirect('http://localhost:8000/users/login');
+            }).catch(function(err){
+
+                console.log('Error in creating user while signing up');
+            });
+        }
+        else{
+
+            res.redirect('back');
+        }
+    });
+}
+
+//sign in and create a session
+module.exports.createSession = function(req, res){
+
+    //TODO Later
 }
