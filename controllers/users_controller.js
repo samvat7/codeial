c44e411d61ca45e7fee443a2c91df1f61dbdd1ce
samvat7 <samvattiwari_2k19ae053@dtu.ac.in>
2023-08-07 21:12:56 +0530

    updated folders staged for commit after last commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,9 @@
+const passport = require('passport');
 const User = require('../models/users');
 
 module.exports.users = function(req,res){
+
+    console.log(req);
 
     res.render('users', {
 
@@ -8,12 +11,15 @@ module.exports.users = function(req,res){
     });
 }
 
-module.exports.profile = function(req,res){
+module.exports.profile = function(req, res) {
+    
+        console.log(res.locals.user);
 
-    res.render('user_profile', {
-        title: "Profile page"
-    });
-}
+        res.render('user_profile_logged', {
+            title: `${res.locals.user.name}'s Profile`, // res.locals.user passed on by passport local
+            user: res.locals.user
+        });
+};
 
 module.exports.posts = function(req,res){
 
@@ -45,29 +51,36 @@ module.exports.create = function(req,res){
         return res.redirect('back');
     }
 
-    User.findOne({email: req.body.email}).then(function(err, user){
-
-        if(err){ console.log('Error finding user in signing up.'); return}
-
-        if(!user){
-
-            User.create(req.body).then(function(err, user){
-
+    User.findOne({ email: req.body.email }).then(function(user){
+        if (!user) {
+            User.create(req.body).then(function(newUser) {
                 return res.redirect('http://localhost:8000/users/login');
-            }).catch(function(err){
-
+            }).catch(function(err) {
                 console.log('Error in creating user while signing up');
             });
-        }
-        else{
+        } else {
 
+            console.log('User with this email already exists');
             res.redirect('back');
         }
+    }).catch(function(err) {
+        console.log('Error finding user in signing up.', err);
     });
-}
+};
 
-//sign in and create a session
-module.exports.createSession = function(req, res){
+module.exports.logout = function(req,res){
 
-    //TODO Later
+    req.logout(function(err) {
+        if (err) {
+            console.log('Error logging out:', err);
+            return res.redirect('/');
+        }
+        // Redirect to the home page or any other page you want after successful logout
+        return res.redirect('/');
+    });
+};
+
+module.exports.createSession = function(req,res){
+
+    return res.redirect('http://localhost:8000/users/profile');
 }
