dcc20a2de4522eb333380961c0228801bebb8184
samvat7 <samvattiwari_2k19ae053@dtu.ac.in>
2023-08-07 21:18:15 +0530

    added passport js auth

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
+const passport = require('passport');
+
 const usersController = require('../controllers/users_controller'); 
 
 router.get('/', usersController.users);
 
-router.get('/profile', usersController.profile); //mapping the profile route to the users controller
+router.get('/profile', passport.checkAuthentication, usersController.profile); //mapping the profile route to the users controller
 
 router.get('/posts', usersController.posts);
 
+router.get('/register', passport.checkNotAuthenticated , usersController.register);
 
+router.get('/login', passport.checkNotAuthenticated , usersController.login);
+
+router.post('/create', usersController.create);
+
+//use passport as middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: 'http://localhost:8000/users/login'}
+), usersController.createSession);
+
+router.get('/logout', usersController.logout);
 
 module.exports = router;
