1c63d237c157f0cbddaba5a257640e536826986d
samvat7 <samvattiwari_2k19ae053@dtu.ac.in>
2023-08-12 19:16:52 +0530

    added posts to home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
 
     console.log(req.cookies);
 
-    return res.render('home', {
-        title: "Home"
+    //populating the posts array's user attribute
+
+    Post.find({}).populate('user').then((post_list) => {
+
+        console.log('Retrived posts list from DB');
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: post_list
+        });
+    }).catch((err) => {
+
+        console.log('Error retriving posts from DB', err);
+
+        res.redirect('back');
     });
-}
+
+};
--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ const postSchema = new mongoose.Schema({
 
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    },
+    }
 }, {
 
     timestamps: true
